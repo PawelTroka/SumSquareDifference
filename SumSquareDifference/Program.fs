83399550d92aace71b733c55c91ecf45d22f1c6b
Pawel Troka <Pawel.Troka@powel.no>
2017-02-26 21:15:40 +0100

    Add project files.

---
+++ SumSquareDifference/Program.fs
@@ -0,0 +1,22 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+let sqr x = x * x
+
+let sumOfSquares nums =
+    nums
+    |> Seq.map sqr
+    |> Seq.sum
+
+let squareOfSum nums =
+    nums
+    |> Seq.sum
+    |> sqr
+    
+let sumSquareDifference nums =
+    (nums |> squareOfSum) - (nums |> sumOfSquares)
+    
+[<EntryPoint>]
+let main argv =    
+    printfn "%A" ([1..100] |> sumSquareDifference)
+    0 // return an integer exit code
